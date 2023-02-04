d34fb1bb077c933e9952b08dacce813ea77225ca
NathanFirmo <nathanssfirmo@gmail.com>
2023-02-03 21:05:25 -0300

    feat: add scripts

---
+++ http-code-gen.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+
+const swaggerToHttp = async ({ swagger, api = 'auto_generated', host }) => {
+  const workspaceRoot = process.cwd()
+
+  for (const [path, pathData] of Object.entries(swagger.paths)) {
+    for (const [method, methodData] of Object.entries(pathData)) {
+      let fileData = ''
+      fileData += `# ${methodData.description}\n\n`
+      fileData += `${String(method).toUpperCase()} ${host}${path}`
+
+      const refPath =
+        methodData.requestBody?.content?.['application/json']?.schema?.$ref
+      if (refPath) {
+        const [_, componentsName, schemaName, dtoName] = String(refPath).split(
+          '/'
+        )
+        const dto = swagger[componentsName][schemaName][dtoName]
+        const payload = resolvePayload(dto, swagger)
+
+        fileData += '\n\n' + JSON.stringify(payload, null, 2)
+      }
+
+      const fileName = removeSlash(
+        `${path}.${String(method).toUpperCase()}.http`
+      )
+
+      if (!fs.existsSync(`${workspaceRoot}/http_requests/${api}`)) {
+        await fs.promises.mkdir(`${workspaceRoot}/http_requests/${api}`, {
+          recursive: true,
+        })
+      }
+
+      await fs.promises.writeFile(
+        `${workspaceRoot}/http_requests/${api}/${fileName}`,
+        fileData
+      )
+    }
+  }
+}
+
+const removeSlash = (value) => String(value).replace(/\//g, '⧸')
+
+const resolvePayload = (dto, swagger) => {
+  let payload
+  if (dto.type === 'object') {
+    payload = {}
+    for (const [prop, definition] of Object.entries(dto.properties)) {
+      if (definition?.type === 'object' || !!definition.$ref) {
+        const refPath = definition.$ref
+        if (refPath) {
+          const [_, componentsName, schemaName, dtoName] = String(
+            refPath
+          ).split('/')
+          const dto = swagger[componentsName][schemaName][dtoName]
+          payload[prop] = resolvePayload(dto, swagger)
+        }
+      } else if (definition?.type === 'array') {
+        const refPath = definition?.items?.$ref
+        if (refPath) {
+          const [_, componentsName, schemaName, dtoName] = String(
+            refPath
+          ).split('/')
+          const dto = swagger[componentsName][schemaName][dtoName]
+          payload[prop] = [resolvePayload(dto, swagger)]
+        }
+      } else payload[prop] = definition?.type
+    }
+  } else {
+    payload = dto.type
+  }
+
+  return payload
+}
+
+module.exports = swaggerToHttp
---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const httpCodeGen = require('./http-code-gen')
+
+async function main() {
+  const icvRoot = '/home/nathan/Trabalho/Workspace/incentive-me'
+  const apisPath = '/apps/api'
+
+  const apis = await fs.promises.readdir(icvRoot + apisPath)
+  const apisWithOpenApiSpec = apis.filter((api) =>
+    fs.existsSync(icvRoot + apisPath + `/${api}/swagger.json`)
+  )
+
+  let currentMapping = JSON.parse(
+    (await fs.promises.readFile('portsMapping.json')).toString()
+  )
+  const portsMapping = Object.fromEntries(
+    apisWithOpenApiSpec.map((api) => {
+      if (Object.keys(currentMapping).includes(api)) {
+        return Object.entries(currentMapping).find((item) => item[0] === api)
+      }
+      return [api, 'http://localhost:8080']
+    })
+  )
+
+  await fs.promises.writeFile(
+    'portsMapping.json',
+    JSON.stringify(portsMapping, null, 2)
+  )
+
+  for (const api of apisWithOpenApiSpec) {
+    const swaggerFile = await fs.promises.readFile(
+      icvRoot + apisPath + `/${api}/swagger.json`
+    )
+    const swagger = JSON.parse(swaggerFile.toString())
+
+    await httpCodeGen({
+      swagger,
+      api,
+      host: portsMapping[api],
+    })
+  }
+}
+
+main()
